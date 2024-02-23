0a5133d54de6269d70c16a4ed637409705e822af
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-23 18:30:23 -0300

    Add local keyword to local variables

--- maintenance/clean_cache.sh
+++ maintenance/clean_cache.sh
@@ -17,14 +17,14 @@ showLine() {
 cleanAPT() {
     showLine
     echo -e "\n${BOLD_YELLOW}Cleaning APT...${WHITE}"
-    sizeBefore=$(sudo du -sh /var/cache/apt 2>/dev/null | cut -f1)
+    local sizeBefore=$(sudo du -sh /var/cache/apt 2>/dev/null | cut -f1)
     printf "${BOLD_YELLOW}Size Before: %s${WHITE}\n\n" "$sizeBefore"
 
     sudo apt clean
     sudo apt autoremove --purge -y
     sudo apt autoclean -y
 
-    sizeAfter=$(sudo du -sh /var/cache/apt 2>/dev/null | cut -f1)
+    local sizeAfter=$(sudo du -sh /var/cache/apt 2>/dev/null | cut -f1)
     printf "\n${BOLD_YELLOW}Size After: %s${WHITE}\n" "$sizeAfter"
     echo -e "${BOLD_YELLOW}APT Cleaned.${WHITE}\n"
     sleep 1
@@ -33,7 +33,7 @@ cleanAPT() {
 cleanSnap() {
     showLine
     echo -e "\n${BOLD_YELLOW}Cleaning SNAP...${WHITE}"
-    sizeBefore=$(sudo du -h /var/lib/snapd/snaps 2>/dev/null | cut -f1)
+    local sizeBefore=$(sudo du -h /var/lib/snapd/snaps 2>/dev/null | cut -f1)
     printf "${BOLD_YELLOW}Size Before: %s${WHITE}\n\n" "$sizeBefore" 
 
     set -eu
@@ -42,7 +42,7 @@ cleanSnap() {
             sudo snap remove "$snapname" --revision="$revision"
         done
 
-    sizeAfter=$(sudo du -sh /var/lib/snapd/snaps 2>/dev/null | cut -f1)
+    local sizeAfter=$(sudo du -sh /var/lib/snapd/snaps 2>/dev/null | cut -f1)
     printf "\n${BOLD_YELLOW}Size After: %s${WHITE}\n" "$sizeAfter"
     echo -e "${BOLD_YELLOW}SNAP Cleaned${WHITE}.\n"
     sleep 1
@@ -51,12 +51,12 @@ cleanSnap() {
 cleanFlatpak() {
     showLine
     echo -e "\n${BOLD_YELLOW}Cleaning FLATPAK...${WHITE}"
-    sizeBefore=$(du -h /var/tmp/flatpak-cache* 2>/dev/null | awk '{sum+=$1} END {print sum}')
+    local sizeBefore=$(du -h /var/tmp/flatpak-cache* 2>/dev/null | awk '{sum+=$1} END {print sum}')
     printf "${BOLD_YELLOW}Size Before: %s${WHITE}\n\n" "$sizeBefore"
 
     sudo rm -rfv /var/tmp/flatpak-cache-*
 
-    sizeAfter=$(du -h /var/tmp/flatpak-cache* 2>/dev/null | awk '{sum+=$1} END {print sum}')
+    local sizeAfter=$(du -h /var/tmp/flatpak-cache* 2>/dev/null | awk '{sum+=$1} END {print sum}')
     printf "\n${BOLD_YELLOW}Size After: %.2f${WHITE}\n" "$sizeAfter"
     echo -e "${BOLD_YELLOW}FLATPAK Cleaned.${WHITE}\n"
     sleep 1
@@ -65,12 +65,12 @@ cleanFlatpak() {
 cleanThumbnails() {
     showLine
     echo -e "\n${BOLD_YELLOW}Cleaning THUMBNAILS...${WHITE}"
-    sizeBefore=$(sudo du -sh ~/.cache/thumbnails 2>/dev/null | cut -f1)
+    local sizeBefore=$(sudo du -sh ~/.cache/thumbnails 2>/dev/null | cut -f1)
     printf "${BOLD_YELLOW}Size Before: %s${WHITE}\n\n" "$sizeBefore"
 
     sudo rm -rf ~/.cache/thumbnails/*
 
-    sizeAfter=$(sudo du -sh ~/.cache/thumbnails 2>/dev/null | cut -f1)
+    local sizeAfter=$(sudo du -sh ~/.cache/thumbnails 2>/dev/null | cut -f1)
     printf "${BOLD_YELLOW}Size After: %s${WHITE}\n" "$sizeAfter"
     echo -e "${BOLD_YELLOW}THUMBNAILS Cleaned.${WHITE}\n"
     sleep 1
@@ -79,12 +79,12 @@ cleanThumbnails() {
 cleanJournalctl() {
     showLine
     echo -e "\n${BOLD_YELLOW}Cleaning JOURNALCTL...${WHITE}"
-    sizeBefore=$(sudo journalctl --disk-usage 2>/dev/null | cut -f1)
+    local sizeBefore=$(sudo journalctl --disk-usage 2>/dev/null | cut -f1)
     printf "${BOLD_YELLOW}Size Before: %s${WHITE}\n\n" "$sizeBefore"
 
     sudo journalctl --vacuum-time=7d
 
-    sizeAfter=$(sudo journalctl --disk-usage 2>/dev/null | cut -f1)
+    local sizeAfter=$(sudo journalctl --disk-usage 2>/dev/null | cut -f1)
     printf "${BOLD_YELLOW}Size After: %s${WHITE}\n" "$sizeAfter"
     echo -e "${BOLD_YELLOW}JOURNALCTL Cleaned.${WHITE}\n"
     sleep 1
